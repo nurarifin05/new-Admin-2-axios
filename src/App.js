81c42c4807d33336c5204baf5285b378feb2b2f1
nurarifin05 <muhamadnurarifin05@gmail.com>
2021-01-19 10:20:51 +0700

    Reactjs+MaterialUI+Axios+Typescript-1

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import './App.css';
 import LayoutTemplate from './containers/template/Layout';
 import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
 import LoginPage from './containers/template/loginPage';
+import MiniDrawer from './containers/template/tsx';
 
 
 function App() {
   return (
     <Router>
       <Switch>
-      <Route path="/dashboard" component={LayoutTemplate} />
+      <Route path="/dashboard" component={MiniDrawer} />
         <Route path="/" component={LoginPage} />
         
       </Switch>
--- src/components/Pages/menu1.js
+++ src/components/Pages/menu1.js
@@ -1,81 +1,155 @@
-import React, {Component} from 'react'
 
-class Menu1 extends Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            users: [],
-            isLoading: false,
-            isError: false
-        }
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import './style.css'
+
+const URL = 'http://192.168.0.101:5000/api/v1/tr'
+
+const Menu1 = () => {
+    const [employees, setEmployees] = useState([])
+
+  //pengganti componentDidMount
+    useEffect(() => {
+        getData()
+    }, [])
+
+    const getData = async () => {
+        const response = await axios.get(URL)
+        setEmployees(response.data)
+        console.log('ini output res', response)
+    }
+    const removeData = (item_id, e) => {
+        axios.delete(`${URL}/${item_id}`)
+        .then(res => {
+            const del = employees.filter(
+                employee => item_id !== employee.item_id)
+                setEmployees(del)
+        })
     }
 
-    async componentDidMount(){
-        this.setState({isLoading:true})
-        
-        const response = await fetch("http://192.168.0.101:5000/api/v1/tr")
-       
-        if(response.ok){
-            const users = await response.json()
-            console.log(users)
-            this.setState({users,isLoading:false})
-        }else{
-            this.setState({isError:true, isLoading:false})
-        }
+    const renderHeader = () => {
+        let headerElement = ['id', 'Device_ID', 'id_Account','is_location', 'item_id', 'UoM', 'Quantity', 'Line_number', 'Rack_number', 'Bin_number', 'Time_Enter', 'status', 'Action']
+        return headerElement.map((key, index) => {
+            return <th key={index}>{key.toUpperCase()}</th>
+        })
     }
 
-    renderTableHeader = () => {
-        return Object.keys(this.state.users[0]).map(attr => <th key={attr}>
-        {attr.toLocaleUpperCase()}
-        </th>)
-    }
-
-    renderTableRows = () => {
-        return this.state.users.map(user => {
-            return(
-                <tr key={user.id}>
-                    <td>{user.id}</td>
-                    <td>{user.Device_ID}</td>
-                    <td>{user.id_Account}</td>
-                    <td>{user.id_location}</td>
-                    <td>{user.item_id}</td>
-                    <td>{user.UoM}</td>
-                    <td>{user.Quantity}</td>
-                    <td>{user.Line_number}</td>
-                    <td>{user.Rack_number}</td>
-                    <td>{user.Bin_number}</td>
-                    <td>{user.Time_Enter}</td>
-                    <td>{user.status}</td>
-                    
+    const renderBody = () => {
+        return employees && employees.map(({id,Device_ID,id_Account,id_location, item_id, UoM, Quantity, Line_number, Rack_number, Bin_number, Time_Enter, status}) => {
+            return (
+                <tr key={id}>
+                    <td>{id}</td>
+                    <td>{Device_ID}</td>
+                    <td>{id_Account}</td>
+                    <td>{id_location}</td>
+                    <td>{item_id}</td>
+                    <td>{UoM}</td>
+                    <td>{Quantity}</td>
+                    <td>{Line_number}</td>
+                    <td>{Rack_number}</td>
+                    <td>{Bin_number}</td>
+                    <td>{Time_Enter}</td>
+                    <td>{status}</td>
+                     <td className='opration'>
+                    <button className='buttonDelete' onClick={(e) => removeData(item_id, e)}>Delete</button>
+                    <button className='buttonEdit' >Edit</button>
+                    </td> 
                 </tr>
             )
         })
     }
-        render(){
-        const {users, isLoading, isError} = this.state
-        if(isLoading){
-            return <div>Loading</div>
-        }
-        if (isError){
-            return <div>Error</div>
-        }
-
-        return users.length > 0
-        ? (
-            <table>
-                <thead>
-                    <tr>
-                        {this.renderTableHeader()}
-                    </tr>
-                </thead>
-                <tbody>
-                    {this.renderTableRows()}
-                </tbody>
-            </table>
-        ):(
-            <div>No Data </div>
-        )
-    }
+    return (
+        <>
+        <h1 id='title'></h1>
+        <table id='employee'>
+            <thead>
+                <tr>{renderHeader()}</tr>
+            </thead>
+            <tbody>
+                {renderBody()}
+            </tbody>
+        </table>
+        </>
+    )
 }
 
 export default Menu1
+
+// import React from 'react';
+  
+// import axios from 'axios';
+  
+// export default class Menu1 extends React.Component {
+//   state = {
+//     posts: []
+//   }
+  
+//   componentDidMount() {
+//     axios.get(`http://192.168.0.101:5000/api/v1/tr`)
+//       .then(res => {
+//         const posts = res.data;
+//         this.setState({ posts });
+//       })
+//   }
+  
+//   deleteRow(item_id, e){
+//     axios.delete(`http://192.168.0.101:5000/api/v1/tr/${item_id}`)
+//       .then(res => {
+//         console.log(res);
+//         console.log(res.data);
+  
+//         const posts = this.state.posts.filter(item => item.item_id !== item_id);
+//         this.setState({ posts });
+//       })
+  
+//   }
+  
+//   render() {
+//     return (
+//       <div>
+  
+//         <table className="table table-bordered">
+//             <thead>
+//               <tr>
+//                   <th>ID</th>
+//                   <th>Device_ID</th>
+//                   <th>id_Account</th>
+//                   <th>id_location</th>
+//                   <th>item_id</th>
+//                   <th>UoM</th>
+//                   <th>Quantity</th>
+//                   <th>Line_number</th>
+//                   <th>Rack_number</th>
+//                   <th>Bin_number</th>
+//                   <th>Time_Enter</th>
+//                   <th>status</th>
+//               </tr>
+//             </thead>
+  
+//             <tbody>
+//               {this.state.posts.map((post) => (
+//                 <tr>
+//                   <td>{post.id}</td>
+//                   <td>{post.Device_ID}</td>
+//                   <td>{post.id_Account}</td>
+//                   <td>{post.id_location}</td>
+//                   <td>{post.item_id}</td>
+//                   <td>{post.UoM}</td>
+//                   <td>{post.Quantity}</td>
+//                   <td>{post.Line_number}</td>
+//                   <td>{post.Rack_number}</td>
+//                   <td>{post.Bin_number}</td>
+//                   <td>{post.Time_Enter}</td>
+//                   <td>{post.status}</td>
+//                   <td>
+//                     <button className="btn btn-danger" onClick={(e) => this.deleteRow(post.item_id, e)}>Delete</button>
+//                   </td>
+//                 </tr>
+//               ))}
+//             </tbody>
+  
+//         </table>
+//       </div>
+//     )
+//   }
+// }
